d220a197e1600fdddbf14815148092e6889c125a
Alain Cajuste <cajuste.alain@gmail.com>
2023-06-27 21:12:35 +0200

    refactoring: code cleanup and Add isRunning flag to prevent concurrent start calls in SyncProcess.Run

--- domain/repositories/tvtime_repository.go
+++ domain/repositories/tvtime_repository.go
@@ -16,12 +16,10 @@ type TvTimeRepository interface {
 }
 type TvTimeRepositoryParams struct {
 	fx.In
-	Helpers   lib.Helpers
 	TVTimeApi api.TVTimeApi
 	Logger    lib.Logger
 }
 type tvTimeRepository struct {
-	helpers   lib.Helpers
 	tvTimeAPI api.TVTimeApi
 	logger    lib.Logger
 }
@@ -29,14 +27,13 @@ type tvTimeRepository struct {
 // NewTvTimeRepository initialize users repository
 func NewTvTimeRepository(ttrP TvTimeRepositoryParams) TvTimeRepository {
 	return &tvTimeRepository{
-		helpers:   ttrP.Helpers,
 		tvTimeAPI: ttrP.TVTimeApi,
 		logger:    ttrP.Logger,
 	}
 }
 
 func (tR *tvTimeRepository) SearchShow(showName string) ([]entities.TvTimeSearch, error) {
-	names, _ := tR.helpers.FuncNameAndFile()
+	const names = "__tvtime_repository.go__ : SearchShow"
 	shows, err := tR.tvTimeAPI.SearchShow(showName)
 	if err != nil {
 		tR.logger.Error(fmt.Sprintf("%s | %s", names, err))
@@ -46,7 +43,7 @@ func (tR *tvTimeRepository) SearchShow(showName string) ([]entities.TvTimeSearch
 }
 
 func (tR *tvTimeRepository) GetShow(serieID int) (*entities.Show, error) {
-	names, _ := tR.helpers.FuncNameAndFile()
+	const names = "__tvtime_repository.go__ : GetShow"
 	show, err := tR.tvTimeAPI.GetShow(serieID)
 	if err != nil {
 		tR.logger.Error(fmt.Sprintf("%s | %s", names, err))
@@ -57,7 +54,7 @@ func (tR *tvTimeRepository) GetShow(serieID int) (*entities.Show, error) {
 }
 
 func (tR *tvTimeRepository) MarkAsWatched(episodeId int) (bool, error) {
-	names, _ := tR.helpers.FuncNameAndFile()
+	const names = "__tvtime_repository.go__ : MarkAsWatched"
 	good, err := tR.tvTimeAPI.MarkAsWatched(episodeId)
 	if err != nil {
 		tR.logger.Error(fmt.Sprintf("%s | %s", names, err))
--- pkg/lib/helpers.go
+++ pkg/lib/helpers.go
@@ -1,11 +1,6 @@
 package lib
 
 import (
-	"fmt"
-	"path/filepath"
-	"runtime"
-	"strings"
-
 	"go.uber.org/fx"
 )
 
@@ -26,37 +21,6 @@ type HelpersResult struct {
 	fx.Out
 
 	Helpers Helpers
-}
-
-func (h *Helpers) FuncNameAndFile() (string, error) {
-	const names = "__helpers.go__ : FuncNameAndFile"
-	fpcs := make([]uintptr, 1)
-
-	// Skip 2 levels to get the caller
-	n := runtime.Callers(2, fpcs)
-	if n == 0 {
-		h.logger.Error(fmt.Sprintf("%s | %s", names, "MSG: NO CALLER"))
-	}
-
-	caller := runtime.FuncForPC(fpcs[0] - 1)
-	if caller == nil {
-		h.logger.Error(fmt.Sprintf("%s | %s", names, "MSG CALLER WAS NIL"))
-	}
-	// Get the full name of the function
-	fullName := caller.Name()
-
-	// Split the full name into parts
-	parts := strings.Split(fullName, ".")
-
-	// Get the name of the function
-	funcName := parts[len(parts)-1]
-	// Get the file information
-	_, file, _, ok := runtime.Caller(1)
-	if !ok {
-		h.logger.Error(fmt.Sprintf("%s | %s", names, "MSG: Could not get file info"))
-	}
-
-	return fmt.Sprintf("__%s__ : %s", filepath.Base(file), funcName), nil
 }
 
 func NewHelpers(hP HelpersParams) HelpersResult {
